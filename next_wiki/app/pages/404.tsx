f848024b377be42d965fb95cc8fc7c3b458a187d
Andres Prieto <andresprieto2703@gmail.com>
2024-02-14 15:22:44 -0500

    Template structure
    Pasando la arquitectura de UML a paginas de next; como frontend de forma
    simple

---
+++ next_wiki/app/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return (
+        <div>
+            <h2>Parece que lo que está buscando no se pudo encotrar</h2>
+        </div>
+    )
+}
---
+++ next_wiki/app/pages/500.tsx
@@ -0,0 +1,7 @@
+export default function ServerError(){
+    return (
+        <div>
+            <h2>Ups... Parece que el servidor exploto</h2>
+        </div>
+    )
+}
